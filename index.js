073c354f69e05267536bfba5ed3179c90b46f406
Anton Tigunov <seyanaracore@gmail.com>
2022-03-14 22:41:45 +0300

    regExp

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import { Stream } from "stream";
 import { fileName, findingIps } from "./Utils/constants.js";
 
 const logReadStream = fs.createReadStream(fileName, "utf-8");
@@ -7,32 +6,21 @@ const logReadStream = fs.createReadStream(fileName, "utf-8");
 const logWriteStreams = findingIps.map((ip) => {
    return {
       ip,
-      stream: fs.createWriteStream(ip + "_requests.log", {
+      writeStream: fs.createWriteStream(ip + "_requests.log", {
          flags: "a",
          encoding: "utf8",
       }),
    };
 });
 
-const filterStream = new Stream.Transform({
-   transform(chunk, encoding, callback) {
-      logWriteStreams.some((streamEl) => {
-         if (chunk.toString().includes(streamEl.ip)) {
-            this.push(chunk);
-            writeToFile(streamEl.stream, chunk);
-            return true;
-         }
-      });
+const getRegExp = (startWord) => new RegExp(`${startWord}.*\n`, "g");
 
-      callback();
-   },
-});
+logReadStream.on("data", (chunk) => {
+   const chunkString = chunk.toString();
 
-const writeToFile = (stream, chunk) => {
-   stream.write(chunk, (err) => {
-      console.log(err);
+   logWriteStreams.forEach((stream) => {
+      const regExp = getRegExp(stream.ip);
+      const handledString = chunkString.match(regExp).join("");
+      stream.writeStream.write(handledString);
    });
-};
-
-logReadStream.pipe(filterStream).pipe(process.stdout);
-// console.log(logWriteStreams);
+});
