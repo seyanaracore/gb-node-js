2a515c7664299e3dd3c670fb39399b53ac1e857d
Anton Tigunov <seyanaracore@gmail.com>
2022-03-30 12:01:51 +0300

    filehandler

--- Components/fileHandler.js
+++ Components/fileHandler.js
@@ -1,7 +1,20 @@
+import fs from "fs";
 import getRegExp from "./getRegExp";
 
 const fileHandler = (path, stringsList) => {
    const patternsList = stringsList.map((string) => getRegExp(string));
+   const readStream = fs.createReadStream(path);
 };
 
 export default fileHandler;
+
+readStream.on("data", (chunk) => {
+   const chunkString = chunk.toString();
+
+   logWriteStreams.forEach((stream) => {
+      F;
+      const regExp = getRegExp(stream.ip);
+      const handledString = chunkString.match(regExp).join("");
+      stream.writeStream.write(handledString);
+   });
+});
